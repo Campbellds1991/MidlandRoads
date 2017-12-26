be122a68a921376cff6c5abc20a0277f70c1be3c
Campbellds1991 <campbel.ds@live.com>
2017-12-26 10:32:36 -0500

    Update Snow Animation.js

--- Snow Animation.js
+++ Snow Animation.js
@@ -25,7 +25,7 @@ window.onload = function () {
 	function draw() {
 		ctx.clearRect(0, 0, W, H);
 
-		ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
+		ctx.fillStyle = "rgba(172,215,251, 0.6)";
 		ctx.beginPath();
 		for (var i = 0; i < mp; i++) {
 			var p = particles[i];

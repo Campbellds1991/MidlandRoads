3306af6228d8349e9f1ca72fb6d48152b9137dcf
Devin Campbell <campbel.ds@live.com>
2017-12-26 10:07:50 -0500

    Changed Flake Color to Black

--- Snow Animation.js
+++ Snow Animation.js
@@ -25,7 +25,7 @@ window.onload = function () {
 	function draw() {
 		ctx.clearRect(0, 0, W, H);
 
-		ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+		ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
 		ctx.beginPath();
 		for (var i = 0; i < mp; i++) {
 			var p = particles[i];
